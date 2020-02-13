4b466d74add6b91593360ff0f560c94571cbf20b
omarin39 <omaralvarez01.oaa@gmail.com>
2020-02-12 20:04:08 -0600

    incio Proyecto TotalPlay

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
+Vue.prototype.$eventHub = new Vue();
 
 const routes = [
   {
